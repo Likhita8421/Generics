9c2218ac36bf1c251e2766ecfb3c60b4a0cd3fe6
Likhita <leenupandagre24@gmail.com>
2022-06-20 18:34:48 +0530

    initial commit

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Generics
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            //int[] intArry = { 1, 2, 3, 4, 5, 6 };
+            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4, 5.5 };
+            char[] charArray = { 'L', 'E', 'E', 'N', 'U' };
+
+            //program.toprint(intArray);
+            program.toprint(doubleArray);
+            program.toprint(charArray);
+
+        }
+
+        public static void toprint(double[] inputArry)
+        {
+            foreach (double element in inputArry)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("----------------------");
+        }
+        public static void toprint(char[] inputArry)
+        {
+            foreach (char element in inputArry)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("----------------------");
+        }
+    }
+
+}
